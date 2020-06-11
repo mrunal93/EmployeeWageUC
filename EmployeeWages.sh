ad7461dea8853e0c6a12199ff1c3a57c2a0deddd
mrn26893@gmail.com <mrn26893@gmail.com>
2020-06-11 20:40:32 +0530

    welcome

--- EmployeeWages.sh
+++ EmployeeWages.sh
@@ -1,24 +1,3 @@
 #!/bin/bash
-echo "welcome to empwage computation";
+echo "welcome to empwage computation"
 
-isPresent=$(( RANDOM  % 3));
-<<<<<<< HEAD
-dailyWage=0;
-hoursOfWork=0;
-wagePerHour=20;
-=======
->>>>>>> 57b5713d26111995c2e1c6f23facedca584ee647
-if (( $isPresent == 0)); then
-    echo "not present";
-else
-    echo "present";
-fi
-<<<<<<< HEAD
-
-if (( $isPresent != 0)); then
-    hoursOfWork=8;
-    dailyWage=$(( $hoursOfWork * $wagePerHour));
-fi
-echo $dailyWage;
-=======
->>>>>>> 57b5713d26111995c2e1c6f23facedca584ee647
