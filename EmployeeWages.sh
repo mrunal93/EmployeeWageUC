7590f7ef3bc4a7cab7ff4b3475c77c68db38b733
mrn26893@gmail.com <mrn26893@gmail.com>
2020-06-11 10:38:08 +0530

    Dailywages

--- EmployeeWages.sh
+++ EmployeeWages.sh
@@ -1,3 +1,18 @@
 #!/bin/bash
 echo "welcome to empwage computation";
 
+isPresent=$(( RANDOM  % 3));
+dailyWage=0;
+hoursOfWork=0;
+wagePerHour=20;
+if (( $isPresent == 0)); then
+    echo "not present";
+else
+    echo "present";
+fi
+
+if (( $isPresent != 0)); then
+    hoursOfWork=8;
+    dailyWage=$(( $hoursOfWork * $wagePerHour));
+fi
+echo $dailyWage;
